1e802f9544520febe600b77a1df0b3a2d09f240d
Gustavo Neumann <gneumann7164@gmail.com>
2024-09-02 12:10:50 -0300

    Insercao

--- script.py
+++ script.py
@@ -4,6 +4,7 @@ import json
 import time
 import random
 import logging
+import pyodbc
 
 # Configura o logger
 logging.basicConfig(
@@ -11,6 +12,17 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s'
 )
+
+# Configurações do banco de dados
+conn_str = (
+    "DRIVER={SQL Server};"
+    "SERVER=www.thyagoquintas.com.br,1433;"
+    "DATABASE=AUTO;"
+    "UID=guilherme_gustavo;"
+    "PWD=123456789;"
+)
+conn = pyodbc.connect(conn_str)
+cursor = conn.cursor()
 
 # Caminho do arquivo de checkpoint
 checkpoint_file = 'checkpoint.json'
@@ -73,6 +85,83 @@ def get_last_page():
     
     return last_page + 1
 
+def save_data_to_db(car):
+    try:
+        unique_id = car['UniqueId']
+        title = car['Specification']['Title']
+        make = car['Specification']['Make']['Value']
+        model = car['Specification']['Model']['Value']
+        version = car['Specification']['Version']['Value']
+        year_fabrication = car['Specification']['YearFabrication']
+        year_model = car['Specification']['YearModel']
+        odometer = car['Specification']['Odometer']
+        transmission = car['Specification']['Transmission']
+        number_ports = int(car['Specification']['NumberPorts']) if car['Specification']['NumberPorts'] else None
+        body_type = car['Specification']['BodyType']
+        vehicle_attributes = ", ".join([attr['Name'] for attr in car['Specification']['VehicleAttributes']])
+        armored = 1 if car['Specification']['Armored'] == "S" else 0
+        color = car['Specification']['Color']['Primary']
+        seller_type = car['Seller']['SellerType']
+        seller_city = car['Seller']['City']
+        seller_state = car['Seller']['State']
+        tipo_estabelecimento = car['Seller']['AdType']['Value'] if 'AdType' in car['Seller'] and 'Value' in car['Seller']['AdType'] else None
+        seller_fantasy_name = car['Seller']['FantasyName']
+        car_state = car['Details']['State']
+        car_city = car['Seller']['Localization'][0]['City']
+        car_country = car['Seller']['Localization'][0]['Country']
+        car_neighborhood = car['Seller']['Localization'][0]['Neighborhood']
+        car_zip_code = car['Seller']['Localization'][0]['ZipCode']
+        car_full_address = car['Seller']['Localization'][0]['AbbrState']
+        price_webmotors = car['Prices']['Price']
+        price_fipe = car['Details']['FipePrice']
+        highest_price = car['Details']['BiggestPrice']
+        lowest_price = car['Details']['SmallestPrice']
+        average_price = car['Details']['MediumPrice'] 
+        car_description = car['LongComment']
+        date_update_fipe = car['Details']['UpdateDateFipe'] if 'UpdateDateFipe' in car['Details'] else None
+        date_update_webmotors = car['Details']['UpdateDateWebmotorsTable'] if 'UpdateDateWebmotorsTable' in car['Details'] else None
+
+        cursor.execute("""
+            INSERT INTO AUTO.dbo.carros (
+                unique_id, titulo_carro, marca, modelo, versao, ano_fabricacao, ano_modelo, rodometro,
+                cambio_descricao, numero_portas, carroceria_descricao, atributos_veiculo, blindado, cor,
+                tipo_vendedor, cidade_vendedor, estado_vendedor, tipo_estabelecimento, nome_fantasia, estado_carro,
+                cidade_carro, pais_carro, bairro_carro, cep_carro, endereco_completo_carro, preco_webmotors,
+                preco_fipe, maior_preco_encontrado, menor_preco_encontrado, preco_procurado, carro_descricao,
+                data_update_fipe, data_update_webmotors
+            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        """, (
+            unique_id, title, make, model, version, year_fabrication, year_model, odometer,
+            transmission, number_ports, body_type, vehicle_attributes, armored, color,
+            seller_type, seller_city, seller_state, tipo_estabelecimento, seller_fantasy_name, car_state,
+            car_city, car_country, car_neighborhood, car_zip_code, car_full_address, price_webmotors,
+            price_fipe, highest_price, lowest_price, average_price, car_description,
+            date_update_fipe, date_update_webmotors))
+
+        conn.commit()
+        logging.info(f"Carro com UniqueId {unique_id} salvo no banco de dados.")
+        return True
+
+    except Exception as e:
+        logging.error(f"Erro ao salvar carro no banco de dados: {e}")
+        conn.rollback()
+        return False
+
+def process_and_save_page_data(page_folder):
+    # Junta todos os arquivos JSON da página em uma única lista
+    all_cars = []
+    for json_file in os.listdir(page_folder):
+        if json_file.endswith('.json'):
+            json_path = os.path.join(page_folder, json_file)
+            with open(json_path, 'r', encoding='utf-8') as f:
+                car_data = json.load(f)
+                all_cars.append(car_data)
+
+    # Salva os dados coletados no banco de dados
+    if all_cars:
+        for car in all_cars:
+            save_data_to_db(car)
+
 def main():
     checkpoint = load_checkpoint()
     if checkpoint:
@@ -133,6 +222,9 @@ def main():
             sleep_time_car = random.randint(5, 10)
             logging.info(f"Aguardando {sleep_time_car} segundos antes de buscar o próximo carro...")
             time.sleep(sleep_time_car)
+
+        # Processa e salva os dados da página inteira no banco de dados
+        process_and_save_page_data(page_folder)
 
         # Verifica se todos os carros da página foram processados
         if index == total_cars:
